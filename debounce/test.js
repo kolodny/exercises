58bbf347d0bb2047f96c2d956a7ef52168a5d86b
kolodny (Moshe Kolodny) <kolodny.github@gmail.com>
2015-06-14 11:08:24 -0400

    change to debounce TODO: add proper throttle

--- debounce/test.js
+++ debounce/test.js
@@ -1,15 +1,25 @@
 var assert = require('assert');
-var throttle = require('./');
+var debounce = require('./');
 
-describe('throttle', function() {
-  it("won't execute more than once within the threshold", function(done) {
-    var called = 0;
-    var throttled = throttle(function() {
+describe('debounce', function() {
+  it('waits for the threshold to pass before executing', function(done) {
+    var now = new Date();
+    var debounced = debounce(function() {
+      assert.equal(new Date() - now > 10);
+      done();
       called++;
     }, 10);
-    throttled();
-    throttled();
-    throttled();
+    debounced();
+  });
+
+  it("won't execute more than once within the threshold", function(done) {
+    var called = 0;
+    var debounced = debounce(function() {
+      called++;
+    }, 10);
+    debounced();
+    debounced();
+    debounced();
     setTimeout(function() {
       assert.equal(called, 1);
       done();
@@ -18,12 +28,12 @@ describe('throttle', function() {
 
   it("will execute more than once outside the threshold", function(done) {
     var called = 0;
-    var throttled = throttle(function() {
+    var debounced = debounce(function() {
       called++;
     }, 10);
-    throttled();
-    setTimeout(throttled, 5);
-    setTimeout(throttled, 20);
+    debounced();
+    setTimeout(debounced, 5);
+    setTimeout(debounced, 20);
     setTimeout(function() {
       assert.equal(called, 2);
       done();
@@ -32,26 +42,26 @@ describe('throttle', function() {
 
   it('gets called with context', function(done) {
     var ctx;
-    var throttled = throttle(function() {
+    var debounced = debounce(function() {
       ctx = this;
     }, 10);
-    throttled.call(11);
-    throttled.call(22);
+    debounced.call(11);
+    debounced.call(22);
     setTimeout(function() {
-      assert.equal(ctx, 11);
+      assert.equal(ctx, 22);
       done();
     }, 15)
   });
 
   it('gets called with arguments', function(done) {
     var args;
-    var throttled = throttle(function() {
+    var debounced = debounce(function() {
       args = [].slice.call(arguments);
     }, 10);
-    throttled(11, 22, 33);
-    throttled(22, 33, 44);
+    debounced(11, 22, 33);
+    debounced(22, 33, 44);
     setTimeout(function() {
-      assert.deepEqual(args, [11, 22, 33]);
+      assert.deepEqual(args, [22, 33, 44]);
       done();
     }, 15);
   });
