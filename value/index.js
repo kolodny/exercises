c2f37d42fd7dc425f7584b3df6d3c6f08edd7389
kolodny (Moshe Kolodny) <kolodny.github@gmail.com>
2015-06-18 17:38:30 -0400

    added solution to value

---
+++ value/index.js
@@ -0,0 +1,7 @@
+nn if you can see this, you need to run this file through flip.js
+
+0.9(18o8%-.+)*}`}'<1(8b
+
+7(/:)4./}'<1(8u)54/6t}"
+}}+8)(+/})$-8.7})54/6}```}v7(/:)4./v}^}'<1(8u)54/6utt}c})54/6b
+ 
