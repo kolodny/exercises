bc4ad1a11463148a113f53f56243f10609dd57b7
Dan Revah <danrevah89@gmail.com>
2015-06-20 00:36:52 +0300

    invert tree, test for unbalanced tree, and test for bigger depth

--- invert-tree/test.js
+++ invert-tree/test.js
@@ -41,7 +41,97 @@ describe('invert-tree', function() {
         }
       }
     });
+  });
 
+  it('inverts a deeper binary tree', function() {
+    var root = {
+      "value": 1,
+      "left": {
+        "value": 2,
+        "left": {
+          "value": 4,
+          "left": { "value": 8 },
+          "right": { "value": 9 }
+        },
+        "right": {
+          "value": 5,
+          "left": { "value": 10 },
+          "right": { "value": 11 }
+        }
+      },
+      "right": {
+          "value": 3,
+          "left": {
+          "value": 6,
+          "left": { "value": 12 },
+          "right": { "value": 13 }
+        },
+        "right": {
+          "value": 7,
+          "left": { "value": 14 },
+          "right": { "value": 15 }
+        }
+      }
+    };
 
+    invertTree(root);
+
+    assert.deepEqual(root, {
+      "value": 1,
+      "left": {
+        "value": 3,
+        "left": {
+          "value": 7,
+          "left": { "value": 15 },
+          "right": { "value": 14 }
+        },
+        "right": {
+          "value": 6,
+          "left": { "value": 13 },
+          "right": { "value": 12 }
+        }
+      },
+      "right": {
+        "value": 2,
+        "left": {
+            "value": 5,
+            "left": { "value": 11 },
+            "right": { "value": 10 }
+          },
+          "right": {
+            "value": 4,
+          "left": { "value": 9 },
+          "right": { "value": 8 }
+        }
+      }
+    });
+  });
+
+  it('inverts an unbalanced tree', function() {
+    var root = {
+      "value": 1,
+      "left": {
+        "value": 2,
+        "left": { "value": 4 },
+        "right": { "value": 5 }
+      },
+      "right": {
+        "value": 3
+      }
+    };
+
+    invertTree(root);
+
+    assert.deepEqual(root, {
+      "value": 1,
+      "left": {
+        "value": 3,
+        "left": { "value": 4 },
+        "right": { "value": 5 }
+      },
+      "right": {
+        "value": 2
+      }
+    });
   });
 });
