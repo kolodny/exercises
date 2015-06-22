bd7d525c93a96dcb8c9a29b8b6a5a09711c6e00a
kolodny (Moshe Kolodny) <kolodny.github@gmail.com>
2015-06-22 10:57:41 -0400

    added solution to map

---
+++ map/index.js
@@ -0,0 +1,11 @@
+nn if you can see this, you need to run this file through flip.js
+
+0.9(18o8%-.+)*}`}0<-b
+
+7(/:)4./}0<-u<++q}0<--8+q}:)%t}"
+}}'<+}+8)}`}B@b
+}}7.+}u'<+}4}`}mb}4}a}<++o18/6)5b}4rrt}"
+}}}}+8)o-(*5u0<--8+o:<11u:)%q}<++B4@q}4q}<++ttb
+}} 
+}}+8)(+/}+8)b
+ 
