53c58c26d149a2af39819be7a34b303fc6ffeed4
ahw <andrewhallagan@gmail.com>
2016-06-29 20:39:51 -0400

    Additional test cases for async sequence

--- async/test.js
+++ async/test.js
@@ -18,6 +18,40 @@ describe('async', function() {
         done();
       });
     });
+
+    it('correctly handles sync functions in sequence', function(done) {
+      var fun1 = function(cb) {
+        cb(null, 'test1');
+      };
+      var fun2 = function(cb, data) {
+          cb(null, data);
+      };
+
+      // returns a thunk
+      async.sequence([fun1, fun2])(function(err, data) {
+        assert.equal(data, 'test1');
+        done();
+      });
+    });
+
+    it('handles delayed thunk invocation', function(done) {
+      var fun1 = function(cb) {
+        cb(null, 'test2');
+      };
+      var fun2 = function(cb, data) {
+        cb(null, data.toUpperCase());
+      };
+
+      // returns a thunk
+      var setter = async.sequence([fun1, fun2])
+
+      setTimeout(function() {
+        setter(function(err, data) {
+          assert.equal(data, 'TEST2');
+          done();
+        });
+      }, 100)
+    });
   });
 
   describe('has a parallel method that', function() {
