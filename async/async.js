76117a5b4865dc17c81a63eb451ba516ccacc929
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-24 16:17:58 -0400

    Scaffold out required methods. Include in test.js

---
+++ async/async.js
@@ -0,0 +1,17 @@
+exports.sequence = function(fns) {
+  return function(callback) {
+
+  };
+};
+
+exports.parallel = function(fns) {
+  return function(callback) {
+
+  };
+};
+
+exports.race = function(fns) {
+  return function(callback) {
+
+  };
+};
