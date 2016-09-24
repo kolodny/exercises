2ff808dac9c698327ce669fab34e03a4a6c78d87
Jimmy Harrison <jtharrison91@gmail.com>
2016-09-24 16:31:55 -0400

    Implement sequence method of async challenge.

--- async/async.js
+++ async/async.js
@@ -1,6 +1,22 @@
 exports.sequence = function(fns) {
-  return function(callback) {
-
+  return function(finalCallback) {
+    var index = 0;
+    // Kick off the loop
+    next();
+    // Inner callback for functions in the sequence
+    function next(err, data) {
+      if (err) {
+        // On error, break sequence and call final callback
+        return finalCallback(err, null);
+      }
+      // Call the next function in the sequence, unless there are no more,
+      // in which case call the final callback
+      var method = fns[index] || finalCallback;
+      // Increment index
+      index++;
+      // Execute the next method, passing along the data
+      return method(next, data);
+    };
   };
 };
 
